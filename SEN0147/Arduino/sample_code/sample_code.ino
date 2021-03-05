401a6cd34a115eaef958089d558c80f0e03757f0
KaliChen <huiyichen19830120@gmail.com>
2021-03-05 17:00:54 +0800

    20210305

---
+++ SEN0147/Arduino/sample_code/sample_code.ino
@@ -0,0 +1,23 @@
+
+#define grayscalePin 8
+
+void setup()
+{
+ Serial.begin(9600);
+ pinMode(grayscalePin,INPUT);
+ Serial.println("Smart Grayscale sensor:");
+}
+void loop()
+{
+  Serial.print("Grayscale:");
+  if(digitalRead(grayscalePin))
+  {
+    Serial.println("White detected");
+  }
+  else
+  {
+    Serial.println("Black detected");
+  }
+   
+  delay(500);
+}
